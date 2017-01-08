486003477080cf72df6db1dedc7dd6f81c3b6968
abhitrip <atripath@eng.ucsd.edu>
2017-01-07 22:16:07 -0800

    Adding counts of dig 1 occurence

---
+++ LeetCode/pythonSols/countNumDigOnes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self):
+        pass
+    def countDigitOne(self,n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        i,numOnes = 1,0
+        while i<=n:
+            numOnes = 
+
+        return 168
