7fa94f2c6771bd04773630063ba57f36ef279a75
abhitrip <atripath@eng.ucsd.edu>
2017-01-29 10:46:23 -0800

    Adding linked list cycle

---
+++ LeetCode/pythonSols/LinkedList/cycle2.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head: return None
+        cycle = False
+        slow,fast = head,head
+        while fast and fast.next:
+            slow,fast = slow.next,fast.next.next
+            if slow==fast: 
+                slow = head
+                while slow!=fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+                
+            
+        return None
