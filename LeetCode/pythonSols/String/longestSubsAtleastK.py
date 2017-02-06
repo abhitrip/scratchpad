7febae6aa0c83fbb870857ab913dc4b4fc47c209
abhitrip <atripath@eng.ucsd.edu>
2017-02-06 14:40:02 -0800

    Adding longest substring at least k characters

---
+++ LeetCode/pythonSols/String/longestSubsAtleastK.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        
+        
+        if len(s)<1: return 0
+        dic = collections.Counter(s)
+        mySet = set(i for i in dic.keys() if dic[i]<k)
+        if len(mySet)==0: return len(s)
+        start = 0
+        intervals = []
+        while start<len(s):
+            if s[start] in mySet:
+                start+=1
+            else:
+                i = start
+                while start<len(s) and s[start] not in mySet:
+                    start+=1
+            
+                intervals.append((i,start))
+        maxSub = 0    
+        for start,end in intervals:
+            maxSub = max(self.longestSubstring(s[start:end],k),maxSub)
+        return maxSub
+        
+        
+        
