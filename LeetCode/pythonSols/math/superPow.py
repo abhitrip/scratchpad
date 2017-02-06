cd15a845c02d87d9df21fe517a9f12488b7740e9
abhitrip <atripath@eng.ucsd.edu>
2017-02-05 22:07:25 -0800

    Adding superpow

--- LeetCode/pythonSols/math/superPow.py
+++ LeetCode/pythonSols/math/superPow.py
@@ -1,25 +1,11 @@
 class Solution(object):
-    """
-    This is to find (a^b)%1337, where b is a number written
-    in form of a list
-    """
-    def superPow(self, a, b):
+     def superPow(self, a, b):
         """
         :type a: int
         :type b: List[int]
         :rtype: int
         """
-        def powMod(x,y):
-            """
-            computes x^y%1337
-            """
-            res = 1
-            for i in range(y):
-                res = (res*x)%1337
-            return res
-            
+       
         if not b:
             return 1 
-        dig = b[-1]
-        b.pop()
-        return powMod(self.superPow(a,b),10)*powMod(a,dig)
+        return pow(a,b.pop(),1337)*self.superPow(pow(a,10,1337),b)%1337
